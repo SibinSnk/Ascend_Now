faef8612b47a328a59287e714888ae1551d80f35
Yajnesh <yajnesh.k@pacewisdom.com>
2025-02-25 10:51:40 +0530

    Add AI chat functionality and update requirements for Google Generative AI

--- dashboard/ai_utils.py
+++ dashboard/ai_utils.py
@@ -1,3 +1,6 @@
+import google.generativeai as genai
+
+
 prompt = """You are an agent designed to interact with a School SQL database.
 Given an input question, create a syntactically correct {dialect} query to run, then look at the results of the query and return the answer.
 Unless the user specifies a specific number of examples they wish to obtain, always limit your query to at most {top_k} results.
@@ -17,4 +20,23 @@ To start you should ALWAYS look at the tables in the database to see what you ca
 Do NOT skip this step.
 Then you should query the schema of the most relevant tables."""
 
+
+def ai_chat(dialect: str, top_k: int, query: str):
+    generation_config = {
+            "temperature": 0.8,
+            "max_output_tokens": 2000,
+            "response_mime_type": "text/plain",
+        }
+
+    model = genai.GenerativeModel(
+                model_name="gemini-2.0-flash-exp",
+                generation_config=generation_config,
+                system_instruction=prompt.format(dialect=dialect, top_k=top_k),
+            )
+    response = chat_session.send_message(query)
+    chat_session = model.start_chat(
+            # history=self._generate_history(self.project)
+        )
+    ai_res = response.text
+    return ai_res
 
